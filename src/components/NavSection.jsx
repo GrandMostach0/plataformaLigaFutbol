3ae91735c7eccd91f2663e6990239bc2b6fb5406
GrandMostach0 <kreedlegend0@gmail.com>
2025-05-27 14:54:49 -0600

    estructura del header listo

--- src/components/NavSection.jsx
+++ src/components/NavSection.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import { Link } from "react-router";
 
 function NavSection(){
     return(
-        <h1>soy un NAV</h1>
+        <header className="flex items-center justify-between py-4 px-10">
+            <div className="flex items-center">
+                <h1 className="font-bold text-2xl">Liga de Fútbol</h1>
+                <nav className="ml-10 text-base">
+                    <Link className="item-Nav" to="/ligas">Ligas</Link>
+                    <Link className="item-Nav" to="/equipos">Equipos</Link>
+                    <Link className="item-Nav" to="/partidos">Partidos</Link>
+                    <Link className="item-Nav" to="/clasificacion">Clasificación</Link>
+                </nav>
+            </div>
+
+            <div>
+                <Link className="buttonPrimary bg-slate-200" to="/inscripcion">Inscribe a tu equipo</Link>
+                <Link className="buttonPrimary bg-black text-white" to="/contacto">Contacto</Link>
+            </div>
+        </header>
     );
 }
 
