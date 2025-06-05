e23a3cbd3034c82f1025ce1f00961157f15159b2
GrandMostach0 <kreedlegend0@gmail.com>
2025-06-05 11:05:24 -0600

    pagina para la parte de mostrar informacion del equipo casi listo 80%

--- src/pages/Equipos/componentsEquipos/TablaEstadisticas.jsx
+++ src/pages/Equipos/componentsEquipos/TablaEstadisticas.jsx
@@ -1,6 +1,131 @@
 function TablaEstadisticas(){
     return(
-        <h1>tabla 🙀</h1>
+        <table className="min-w-full text-left border border-gray-200 rounded-lg">
+            <thead className="bg-gray-100">
+                <tr>
+                    <th className="py-3 px-2">#</th>
+                    <th>Jugador</th>
+                    <th>Posición</th>
+                    <th>Edad</th>
+                    <th>Goles</th>
+                    <th>Asist.</th>
+                    <th title="Tarjetas Amarillas">TA</th>
+                    <th title="Tarjetas Rojas">TR</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+                <tr className="border-b border-b-slate-200">
+                    <td className="py-3 px-2">0</td>
+                    <td>Damian Bolio</td>
+                    <td>Delantero</td>
+                    <td>15</td>
+                    <td>8</td>
+                    <td>3</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+            </tbody>
+        </table>
     )
 }
 
